bd5473493dfc3d14953a7b46b4faf12609f5d4c3
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-08-14 15:37:50 +0100

    Updated RStudio client ID for alpha env

--- rules/Multifactor-Google-Authenticator-Do-Not-Rename.js
+++ rules/Multifactor-Google-Authenticator-Do-Not-Rename.js
@@ -1,29 +1,29 @@
 function (user, context, callback) {
-  
+
   var client_with_mfa = [
     // RStudio client id
-    'AsiY4W2JE2d4UOH8FDgUQV9AYAUWwZkk'
+    'rpYFh3GD2vmk13Rtcvre2gZY2dAvdBSS'
   ].indexOf(context.clientID) !== -1;
 
-  var user_with_mfa = user.app_metadata && user.app_metadata.use_mfa;  
-  
+  var user_with_mfa = user.app_metadata && user.app_metadata.use_mfa;
+
   if (
     //!user.impersonated &&
     //user_with_mfa &&
     client_with_mfa
   ) {
-    
+
     context.multifactor = {
       provider: 'google-authenticator',
 
       // optional, the label shown in the authenticator app
-      issuer: 'MOJ Analytical Platform (dev)', 
+      issuer: 'MOJ Analytical Platform (dev)',
 
       // optional, the key to use for TOTP. By default one is generated for you
-      // key: '{YOUR_KEY_HERE}', 
+      // key: '{YOUR_KEY_HERE}',
 
-      // optional, defaults to true. false forces 2FA every time. 
-      allowRememberBrowser: false 
+      // optional, defaults to true. false forces 2FA every time.
+      allowRememberBrowser: false
     };
   }
 
