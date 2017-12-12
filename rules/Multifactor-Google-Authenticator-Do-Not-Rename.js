7b5fefc9245a27db0f7b2f8337691c18186c2a29
Kerin Cosford <cosford@gmail.com>
2017-12-12 10:09:00 +0000

    Enable MFA based on connection type instead of client ID

--- rules/Multifactor-Google-Authenticator-Do-Not-Rename.js
+++ rules/Multifactor-Google-Authenticator-Do-Not-Rename.js
@@ -1,29 +1,25 @@
 function (user, context, callback) {
-  
-  var client_with_mfa = [
-    // RStudio client id
-    'AsiY4W2JE2d4UOH8FDgUQV9AYAUWwZkk'
-  ].indexOf(context.clientID) !== -1;
 
-  var user_with_mfa = user.app_metadata && user.app_metadata.use_mfa;  
-  
-  if (
-    //!user.impersonated &&
-    //user_with_mfa &&
-    client_with_mfa
-  ) {
-    
+  var mfa_enabled_connection = [
+    'github',
+    'google-oauth2'
+  ].indexOf(context.connection) !== -1;
+
+  var user_with_mfa = user.app_metadata && user.app_metadata.use_mfa;
+
+  if (mfa_enabled_connection) {
+
     context.multifactor = {
       provider: 'google-authenticator',
 
       // optional, the label shown in the authenticator app
-      issuer: 'MOJ Analytical Platform (dev)', 
+      issuer: 'MOJ Analytical Platform (dev)',
 
       // optional, the key to use for TOTP. By default one is generated for you
-      // key: '{YOUR_KEY_HERE}', 
+      // key: '{YOUR_KEY_HERE}',
 
-      // optional, defaults to true. false forces 2FA every time. 
-      allowRememberBrowser: false 
+      // optional, defaults to true. false forces 2FA every time.
+      allowRememberBrowser: false
     };
   }
 
