348daf64e469df720db8eec8bada35b0d60f3e77
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-03-07 10:05:08 +0000

    PR amendments: - inverted 2FA exceptions logic for readability - added note on 5 seconds threashold to consider a user logged in - removed `console.log()` - removed spaces in parentheses - removed comments

--- rules/Multifactor-Google-Authenticator-Do-Not-Rename.js
+++ rules/Multifactor-Google-Authenticator-Do-Not-Rename.js
@@ -10,31 +10,27 @@ function (user, context, callback) {
     var MFA_CHALLENGE_EVERY_MINUTES = 8 * 60; // 8 hours
 
 
-    var enabled_for_user = !user.app_metadata || user.app_metadata.use_mfa !== false;
-    var enabled_for_connection = ENABLED_CONNECTIONS.indexOf(context.connection) !== -1;
-    var enabled_for_client = DISABLED_CLIENTS.indexOf(context.clientID) === -1;
+    var disabled_for_user = user.app_metadata && user.app_metadata.use_mfa === false;
+    var disabled_for_connection = ENABLED_CONNECTIONS.indexOf(context.connection) === -1;
+    var disabled_for_client = DISABLED_CLIENTS.indexOf(context.clientID) !== -1;
 
-    if ( !enabled_for_user || !enabled_for_connection || !enabled_for_client ) {
-        console.log('2FA disabled for user, connection or client');
-        return callback(null, user, context);
+    if (disabled_for_user || disabled_for_connection || disabled_for_client) {
+        callback(null, user, context);
     }
 
     getUserProfile(user)
         .then(function (user_profile) {
-            if ( !loggedInRecently(user_profile) ) {
-                console.log('user did not log in recently - enabling MFA');
+            if (!loggedInRecently(user_profile)) {
                 enableMFA(context);
             }
 
-            return callback(null, user, context);
+            callback(null, user, context);
         })
         .catch(function(err) {
             enableMFA(context);
-            return callback(null, user, context);
+            callback(null, user, context);
         });
 
-
-    // Utility functions
 
     function enableMFA(context) {
         context.multifactor = {
@@ -50,6 +46,17 @@ function (user, context, callback) {
         if (user_profile.last_login) {
             var secs_from_last_login = (new Date() - new Date(user_profile.last_login)) / 1000;
 
+            // User goes throught this rule twice apparently.
+            // 1) The first time, if the user didn't log in recently we'll
+            // add the MFA configuration to the context.
+            // 1a) Auth0 updates the user `last_login` even if they were not
+            // challenged for 2FA yet
+            // 2) The second time we'll need to add the MFA configuration to the
+            // context again or they will not be challenged.
+            //
+            // Because we need to account for 1a) we don't consider a login in
+            // the last 5 seconds as a login. That's an arbitrary short number
+            // of seconds to account for the redirect to challenge users for MFA.
             if (secs_from_last_login < 5) {
                 return false;
             }
