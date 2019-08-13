33ed4b384cd39ca310f8c509867d8d8d826f4560
Mikael <mikellallison@gmail.com>
2019-08-13 16:39:44 +0100

    Dev Auth0 Rules
    Adding new branch to be tracked by new dev tenant in auth0

--- rules/add-group-claim-to-token.js
+++ rules/add-group-claim-to-token.js
@@ -1,7 +1,7 @@
 function (user, context, callback) {
 
   var targeted_clients = [
-    'p4L2qRcSgWyqHjoHanJ4QyhWL1iX612i'
+    'shL4AmGXou3jJS7GBosOgXHjVX3Rvvxq'
   ].indexOf(context.clientID) !== -1;
 
   var github_identity = _.find(user.identities, {connection: 'github'});
@@ -9,7 +9,7 @@ function (user, context, callback) {
   if (github_identity && targeted_clients) {
     // For custom claims, you must define a namespace for oidc compliance.
     // See https://auth0.com/docs/api-auth/tutorials/adoption/scope-custom-claims
-    var namespace = 'https://api.dev.mojanalytics.xyz/claims/';
+    var namespace = 'https://api.dev.analytical-platform.service.justice.gov.uk/claims/';
     var options = {
       url: 'https://api.github.com/user/teams',
       headers: {
